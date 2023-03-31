3175cb40ac2e49e6e70b662b4bdd24da1ff0beb9
viktor_wsl <viktor3210@gmail.com>
2023-03-31 22:14:03 +0200

    fixed bug when saving file

--- parser_diag.py
+++ parser_diag.py
@@ -40,4 +40,4 @@ elif conf["model"] == "aubry_andre":
 
     value = eigen_aubryandre(L, lambd, t, gamma_1L, gamma_2L, gamma_1R, gamma_2R, eigvecs)
 
-    np.save(f"data_aubryandre/L{L}/W{W}/aubryandre_ind{args.index}", value)
+    np.save(f"data_aubryandre/L{L}/lambd{lambd}/aubryandre_ind{args.index}", value)
