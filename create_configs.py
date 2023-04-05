8ceea70756bcecd094ffefe4c0dd0a037fe6d73b
viktor_wsl <viktor3210@gmail.com>
2023-04-05 20:20:54 +0200

    now writes configs properly

--- create_configs.py
+++ create_configs.py
@@ -19,17 +19,21 @@ parser.add_argument('--G2R', type=float, nargs='?', default=0)    # Bath couplin
 parser.add_argument('--vectors', action='store_true')             # Whether to compute eigenvectors or not
 args = parser.parse_args()
 
-if not os.path.exists("configs"):
-    os.makedirs("configs")
+if not os.path.exists(f'configs/{args.sim_name}_configs'):
+    os.makedirs(f'configs/{args.sim_name}_configs')
 
-
+count = 0
 for L_ind, L in enumerate(args.length):
     for W in args.disorder:
-        config = {}
+        config = {'length': L, 'disorder': W, 'reals': args.reals[L_ind]}
         for arg in vars(args):
-            config[arg] = getattr(args, arg)
-
+            if arg != 'length' and arg != 'disorder' and arg != 'reals':
+                config[arg] = getattr(args, arg)
+        with open(f'configs/{args.sim_name}_configs/{args.sim_name}_conf{count}.toml', 'wb') as f:
+            tomli_w.dump(config, f)
+        count += 1
+            
 for L in args.length:
     for W in args.disorder:
-        if not os.path.exists(f"{args.sim_name}/L{L}/W{W}"):
-            os.makedirs(f"{args.sim_name}/L{L}/W{W}")
+        if not os.path.exists(f'data/{args.sim_name}_data/L{L}/W{W}'):
+            os.makedirs(f'data/{args.sim_name}_data/L{L}/W{W}')
